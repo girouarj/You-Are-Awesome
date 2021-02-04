5f152ab3e4fdc2393ff286b0d04dd2586abfcad7
Jacqueline Girouard <girouarj@bc.edu>
2021-02-03 20:52:57 -0500

    2.9

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
+    var imageNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("viewDidLoad did run!")
@@ -19,23 +21,34 @@ class ViewController: UIViewController {
     }
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        let awesomeMessage = "You Are Awesome!"
-        let greatMessage = "You Are Great!"
-        let bMessage = "You Are Da Bomb!"
         
-        if messageLabel.text == awesomeMessage {
-            messageLabel.text = greatMessage
-            messageLabel.textColor = UIColor.blue
-            imageView.image = UIImage(named: "image0")
-        } else if  messageLabel.text == greatMessage {
-            messageLabel.text = bMessage
-            messageLabel.textColor = UIColor.red
-            imageView.image = UIImage(named: "image1")
-        } else {
-            messageLabel.text = awesomeMessage
-            messageLabel.textColor = UIColor.green
-            imageView.image = UIImage(named: "image2")
+        let imageName = "image" + String(imageNumber)
+        imageView.image = UIImage(named: imageName )
+        imageNumber = imageNumber + 1
+        if imageNumber == 10 {
+            imageNumber = 0
         }
+        
+        
+//        let awesomeMessage = "You Are Awesome!"
+//        let greatMessage = "You Are Great!"
+//        let bMessage = "You Are Da Bomb!"
+//
+//        if messageLabel.text == awesomeMessage {
+//            messageLabel.text = greatMessage
+//            messageLabel.textColor = UIColor.blue
+//            imageView.image = UIImage(named: "image0")
+//        } else if  messageLabel.text == greatMessage {
+//            messageLabel.text = bMessage
+//            messageLabel.textColor = UIColor.red
+//            imageView.image = UIImage(named: "image1")
+//        } else {
+//            messageLabel.text = awesomeMessage
+//            messageLabel.textColor = UIColor.green
+//            imageView.image = UIImage(named: "image2")
+//        }
+        
+        
     }
     
 }
